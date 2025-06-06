7f81ff93c5d34389045b9719fee0a6288c06242f
Pukkok <krystalgggg@gmail.com>
2025-06-06 16:24:29 +0900

    [Feat] 하트아이콘 액티브 기능 추가

--- src/HeartIcon.jsx
+++ src/HeartIcon.jsx
@@ -1,19 +1,22 @@
-export const HeartIcon = ({ size = 24, className = '', style, ...props }) => (
+export const HeartIcon = ({ size = 24, isActive = false, className = '', style, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
     height={size}
-    fill="currentColor"
     viewBox="0 0 24 24"
-    style={style}
+    fill={isActive ? 'currentColor' : 'none'}
+    stroke={'currentColor'}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className={className}
+    style={style}
     {...props}
   >
-    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
-      4.42 3 7.5 3c1.74 0 3.41 0.81 
-      4.5 2.09C13.09 3.81 14.76 3 
-      16.5 3 19.58 3 22 5.42 22 
-      8.5c0 3.78-3.4 6.86-8.55 
-      11.54L12 21.35z"/>
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
+             2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09
+             C13.09 3.81 14.76 3 16.5 3
+             19.58 3 22 5.42 22 8.5
+             c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
   </svg>
 )
